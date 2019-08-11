156b8a9b25fe628679520a3af28bd75ba24df749
Rodrigo <msn_wandscheer@hotmail.com>
2019-08-10 22:25:43 -0300

    Feito exercicio 1_12 3

--- Aula_30_7/src/br/udc/exercicios/Exercicio1_12_2.java
+++ Aula_30_7/src/br/udc/exercicios/Exercicio1_12_2.java
@@ -1,49 +1,28 @@
 package br.udc.exercicios;
 
+import br.udc.exercicios.Exercicio1_12_1.Data;
+import br.udc.exercicios.Exercicio1_12_1.Hora;
+
 public class Exercicio1_12_2 {
-	public class Data{
-		int dia, mes, ano;
-
-		public void setDia(int dia) {
-			if(!verificaDia(dia))
-				return;
-			this.dia = dia;
-		}
-
-		public void setMes(int mes) {
-			if(!verificaMes(mes))
-				return;
-			this.mes = mes;
-		}
-
-		public void setAno(int ano) {
-			if(!verificaAno(ano))
-				return;
-			this.ano = ano;
-		}
+	public static void main(String[] args) {
+		Exercicio1_12_1 obj = new Exercicio1_12_1();
 		
-		public boolean verificaDia(int dia){
-			if(dia<0)
-				return false;
-			if(dia>31)
-				return false;
-			return true;
-		}
+		Hora hora = obj.new Hora();
+		Hora hora_2 = obj.new Hora();
 		
-		public boolean verificaMes(int mes){
-			if(mes<0)
-				return false;
-			if(mes>12)
-				return false;
-			return true;
-		}
+		hora.setHora(1);
+		hora.setMinutos(2);
 		
-		public boolean verificaAno(int ano){
-			if(ano<0)
-				return false;
-			if(ano>2019)
-				return false;
-			return true;
-		}
-	}
+		hora_2.setHora(1);
+		hora_2.setMinutos(3);
+		
+		Data data = obj.new Data();
+		
+		data.setDia(1);
+		data.setMes(2);
+		data.setAno(3);
+		
+		System.out.println(hora);
+		System.out.println(data);
+	}	
 }
