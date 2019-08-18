130d388cbbe3818c1f6c326cfd84be1e02ae329c
Rodrigo <msn_wandscheer@hotmail.com>
2019-08-18 11:04:09 -0300

    Adicionado exercicio 3 capitulo 7

--- Aula_30_7/src/exercicio_7_1/Aplicacao.java
+++ Aula_30_7/src/exercicio_7_1/Aplicacao.java
@@ -3,9 +3,14 @@ package exercicio_7_1;
 public class Aplicacao {
 
 	public static void main(String[] args) {
+		double pontoX=1, pontoY=2;
+		
 		Ponto2D ponto = new Ponto2D();
 		
+		ponto.setX(pontoX);
+		ponto.setY(pontoY);
 		
+		System.out.println(ponto);
 
 	}
 
--- Aula_30_7/src/exercicio_7_1/Ponto2D.java
+++ Aula_30_7/src/exercicio_7_1/Ponto2D.java
@@ -1,8 +1,8 @@
 package exercicio_7_1;
 
 public class Ponto2D {
-	public double x;
-	public double y;
+	public double x, x2;
+	public double y, y2;
 	
 	public double getX() {
 		return x;
@@ -19,6 +19,36 @@ public class Ponto2D {
 	public void setY(double y) {
 		this.y = y;
 	}
+
+	public double getX2() {
+		return x2*(-1);
+	}
+
+	public double getY2() {
+		return y2*(-1);
+	}
+	
+	public Ponto2D(){
+		this.x=0;
+		this.x2=0;
+		this.y=0;
+		this.y2=0;
+	}
+	
+	public Ponto2D(double x, double x2, double y, double y2){
+		this.x=x;
+		this.x2=x*(-1);
+		this.y=y;
+		this.y2=y*(-1);
+	}
+
+	@Override
+	public String toString() {
+		return "Ponto: ("+x+", "+y+") ("+x2+", "+y2+")";
+	}
+	
+	
+	
 }
 
 
